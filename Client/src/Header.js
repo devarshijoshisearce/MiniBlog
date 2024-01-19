22fbaaa818c4bb8080c5c83dbf6cdf5a11df6222
rachit <rachit.shah@searce.com>
2024-01-19 15:55:55 +0530

    Added a sample blog page

--- Client/src/Header.js
+++ Client/src/Header.js
@@ -1,8 +1,11 @@
 export default function Header(){
     return (
         <header>
-        <a href="" className="logo">Blogging Hub</a>
+        <a href="" className="logo">Logo</a>
+
+        <p><b> Welcome to MiniBlog </b></p>
         <nav>
+       
           <a href="">Login</a>
           <a href="">Register</a>
         </nav>
