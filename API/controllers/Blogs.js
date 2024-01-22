1d3bdf27cba331efb128a1f11df25423a8949c31
Devarshi Joshi <devarshi.joshi@searce.com>
2024-01-22 18:49:51 +0530

    Added deletion API and comments

--- API/controllers/Blogs.js
+++ API/controllers/Blogs.js
@@ -35,10 +35,11 @@ const controller = {
     }
   },
 
+  //API to view all blogs
   async viewBlogs(req, res, next) {
     try {
       // Retrieve all blogs from the database
-      const blogs = await Blog.find();
+      const blogs = await Blog.find();  // true condition, so returns all values
 
       res.status(200).json({ blogs });
     } catch (error) {
@@ -46,22 +47,24 @@ const controller = {
     }
   },
   
+  // API to view a specific blog
   async viewBlogsbyID(req, res, next){
     try{
         // console.log("Temp is ", Temp);
         id = req.params.id
-        const blogs = await Blog.findById(id)
+        const blogs = await Blog.findById(id)   // search the blog ID
         res.status(201).send(blogs);
     } catch (error){
         next(error);
     }
   },
 
+  // API to search blogs by author name -> not that useful
   async viewBlogsbyAuthor(req, res, next)
   {
     try{
         writer = req.params.writer
-        const blogs = await Blog.find({author : writer})
+        const blogs = await Blog.find({author : writer}) // search where author = name given
         res.status(201).send(blogs);
     } catch(error)
     {
@@ -69,19 +72,38 @@ const controller = {
     }
   },
 
+  // API to search blogs by specific author ID
   async viewBlogsbyAuthorID(req, res, next)
   {
     try{
         writerID = req.params.writerid
-        const blogs = await Blog.find({authorID : writerID})
+        const blogs = await Blog.find({authorID : writerID})    // search where authorID = ID given
         res.status(201).send(blogs);
     } catch(error)
     {
         next(error);
     }
+  },
+
+  // API to delete a blog by its ID
+  async deleteBlogsbyID(req, res, next)
+  {
+    try{
+        const { postID, AuthorID, userID } = req.body;
+        if (AuthorID == userID)     //verify that the user is the author
+        {
+            const blogs = await Blog.findByIdAndDelete(postID)      //find the specific post and delete it from the database
+            res.status(201).send(blogs);
+        }
+        else
+        {
+            res.status(400).send("User is not authorized to delete this post")
+        }
+    }catch(error)
+    {
+        next(error);
+    }
   }
-
-
 
 };
 
--- API/routers/Blogs.js
+++ API/routers/Blogs.js
@@ -4,10 +4,11 @@ const router=express.Router()
 
 const controller=require("../controllers/Blogs")
 
-router.post('/createBlog',controller.createBlogs);
-router.get('/viewBlogs',controller.viewBlogs);
-router.get('/viewBlogsbyID/:id',controller.viewBlogsbyID);
-router.get('/viewBlogsbyAuthor/:writer',controller.viewBlogsbyAuthor);
-router.get('/viewBlogsbyAuthorID/:writerid',controller.viewBlogsbyAuthorID);
+router.post('/createBlog',controller.createBlogs);                          //create a blog. data will be passed by a form
+router.get('/viewBlogs',controller.viewBlogs);                              //retrieve all blogs
+router.get('/viewBlogsbyID/:id',controller.viewBlogsbyID);                  //retrieve a specific post. Search by postID
+router.get('/viewBlogsbyAuthor/:writer',controller.viewBlogsbyAuthor);      //retrieve all posts by user name -> search function
+router.get('/viewBlogsbyAuthorID/:writerid',controller.viewBlogsbyAuthorID);//retrieve all post by a specific user -> user page
+router.put('/deleteBlogsbyID/',controller.deleteBlogsbyID);                 //delete a specific post -> pass postID, authorID, userID as params similar to how data was passed in creating a blog
 
 module.exports=router
