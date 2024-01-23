306a5b642ba30f0cecc99cf4dbcdac377c35cac7
Sejal Khilari <sejal.khilari@searce.com>
2024-01-23 17:15:00 +0530

    Integrating signup with backend

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,18 +1,68 @@
+import {useState} from "react";
+
 export default function RegisterPage() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailID, setemailID] = useState('');
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+    const [gender, setGender] = useState('');
+    async function register(ev){
+        ev.preventDefault();
+         await fetch('http://localhost:3000/auth/signup',{
+            method: 'POST',
+            body: JSON.stringify({username,password,emailID,name,number}),
+            headers: {'Content-type':'application/json'},
+        })
+
+    }
     return (
-        <form className="register" action="/login" method="POST">
+        <form className="register" onSubmit={register}>
             <h1>Register</h1>
 
-            <input type="text" name="username" placeholder="Username" />
-            <input type="email" name="email" placeholder="Email" />
-            <input type="text" name="name" placeholder="Name" />
-            <input type="number" name="age" placeholder="Age" />
-            <input type="password" name="password" placeholder="Password" />
-            <label className="gender-1" for="cars">Select your Gender</label>
+            <input 
+            type="text" 
+            name="username" 
+            placeholder="Username" 
+            value={username} 
+            onChange={ev => setUsername(ev.target.value)}/>
+
+            <input 
+            type="emailID" 
+            name="emailID" 
+            placeholder="emailID" 
+            value={emailID}
+            onChange={ev => setemailID(ev.target.value)}/>
+
+            <input 
+            type="password" 
+            name="password" 
+            placeholder="Password" 
+            value={password}
+            onChange={ev => setPassword(ev.target.value)}/>
+
+            <input 
+            type="text" 
+            name="name" 
+            placeholder="Name" 
+            value={name}
+            onChange={ev => setName(ev.target.value)}/>
+
+            <input 
+            type="number" 
+            name="age" 
+            placeholder="Age" 
+            value={number}
+            onChange={ev => setNumber(ev.target.value)}/>
+
+            
+            
+            {/* <label className="gender-1" for="cars">Select your Gender</label>
                 <select name="gender" id="gender">
                     <option value="female">Female</option>
                     <option value="male">Male</option>
-                </select>
+                </select> */}
                 {/* <div >
                 <p> Select your Gender </p>
                     <label for="Male" >Male</label>
