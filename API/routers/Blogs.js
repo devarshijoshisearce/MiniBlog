0fa76c7f0c0479bed35987fd6fc3d9c4534f5fec
Devarshi Joshi <devarshi.joshi@searce.com>
2024-01-24 14:06:34 +0530

    update blog by post ID API done

--- API/routers/Blogs.js
+++ API/routers/Blogs.js
@@ -10,5 +10,6 @@ router.get('/viewBlogsbyID/:id',controller.viewBlogsbyID);                  //re
 router.get('/viewBlogsbyAuthor/:writer',controller.viewBlogsbyAuthor);      //retrieve all posts by user name -> search function
 router.get('/viewBlogsbyAuthorID/:writerid',controller.viewBlogsbyAuthorID);//retrieve all post by a specific user -> user page
 router.put('/deleteBlogsbyID/',controller.deleteBlogsbyID);                 //delete a specific post -> pass postID, authorID, userID as params similar to how data was passed in creating a blog
+router.post('/updateBlogbyID', controller.updateBlogsbyID)                      //update a post -> pass postID, authorID, userID, and the same data as createBLogs.
 
 module.exports=router
