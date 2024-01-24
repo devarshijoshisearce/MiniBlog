0d2c219d74e5bf30eb0e4ab289e7614594a6e46a
Sejal Khilari <sejal.khilari@searce.com>
2024-01-24 10:58:08 +0530

    login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,19 +2,26 @@ import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
 export default function LoginPage() {
-    const [username, setUsername] = useState('');
+    const [emailID, setemailID] = useState('');
     const [password, setPassword] = useState('');
     const [redirect, setRedirect] = useState(false);
 
     async function login(ev){
         ev.preventDefault();
+        console.log("Started")
+        console.log("emailID outside: ", emailID)
+        console.log("password outside: ", password)
         fetch('http://localhost:3000/auth/login',{
             method: 'POST',
-            body: JSON.stringify({username,password}),
+            body: JSON.stringify({emailID,password}),
             headers: {'Content-type':'application/json'},
             credentials: 'include',
-        }).then(res=>{
+        }
+        ).then(res=>{
+            console.log("emailID: ", emailID)
+            console.log("password: ", password)
             if(res.ok){
+                console.log("Login credentials ok.")
                 setRedirect(true);
             } else{      
                 alert("Login Failed - Wrong Credentials")
@@ -23,17 +30,17 @@ export default function LoginPage() {
         
 
         if (redirect) {
-            return <Navigate to="/" />
+            return <Navigate to="/register" />
         }
     }
     return (
         <form className="login" onSubmit={login}>
             <h1>Login</h1>
-            <input type="text" 
-            name="username" 
-            placeholder="Username" 
-            value={username}
-            onChange={ev=>setUsername(ev.target.value)}/>
+            <input type="email" 
+            name="emailID" 
+            placeholder="Email" 
+            value={emailID}
+            onChange={ev=>setemailID(ev.target.value)}/>
 
             <input type="password" 
             name="password" 
