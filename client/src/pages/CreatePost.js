feb5c9f75383cfce8d32708a322085d3a33e5ba7
Indraneela <indraneela.doradla@searce.com>
2024-01-25 17:54:53 +0530

    updating createpost page

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -2,39 +2,49 @@ import { useState } from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
+const modules = {
+  toolbar: [
+    [{ 'header': [1, 2, false] }],
+    ['bold', 'italic', 'underline','strike', 'blockquote'],
+    [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+    ['link', 'image'],
+    ['clean']
+  ],
+};
+const formats = [
+  'header',
+  'bold', 'italic', 'underline', 'strike', 'blockquote',
+  'list', 'bullet', 'indent',
+  'link', 'image'
+];
+
 export default function CreatePost(){
     const [title,setTitle] = useState('');
     const [summary,setSummary] = useState('');
     const [content, setContent] = useState('');
-    const modules = {
-        toolbar: [
-          [{ 'header': [1, 2, false] }],
-          ['bold', 'italic', 'underline','strike', 'blockquote'],
-          [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-          ['link', 'image'],
-          ['clean']
-        ],
-      };
-    const formats = [
-        'header',
-        'bold', 'italic', 'underline', 'strike', 'blockquote',
-        'list', 'bullet', 'indent',
-        'link', 'image'
-      ];
+    
+    function createNewPost(ev){
+      const data = new FormData();
+      data.set('title',title);
+      ev.preventDefault();
+      fetch('http://localhost:3000/auth/post',)
+    }
+
+    
     return (
         // gotta add route in app.js (1:47:30)
-        <form>
+        <form onSubmit={createNewPost}> 
             <input type="title" 
             placeholder={'Title'}
             value={title}
             onChange={ev=> setTitle(ev.target.value)}/>
             <input type="summary" 
             placeholder={"Summary"}
-            value={title}
+            value={summary}
             onChange={ev=> setSummary(ev.target.value)}/>
             <input type="file" />
             <ReactQuill value={content} 
-            onChange={ev=> setContent(ev.target.value)} 
+            onChange={newValue=> setContent(newValue)} 
             modules={modules} 
             formats={formats}/>
             <button style={{marginTop:'5px'}}>Create Post</button>
