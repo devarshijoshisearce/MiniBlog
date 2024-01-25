3ccfe36d480052c24063fd3dbf14e450865e9cd5
rachit <rachit.shah@searce.com>
2024-01-25 11:28:04 +0530

    header-name-functionality

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,15 +1,47 @@
+import { useContext, useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
-
+import { UserContext } from './UserContext';
 export default function Header(){
+    // const {setUserInfo,userInfo} = useContext(UserContext);
+    const {setUserInfo,userInfo} = useContext(UserContext);
+    useEffect(()=>{
+      fetch('https://localhost:4000/profile',{
+        credentials: 'include',
+      }).then(response=>{
+          response.json().then(userInfo=>{
+          // setUserInfo(userInfo);
+          setUserInfo(userInfo);
+          });
+      });
+    },[]);
+    function logout(){
+      fetch('https://localhost:4000/logout',{
+        credentials: 'include',
+        method:'POST'
+      });
+      // setUserInfo(null);
+      setUserInfo(null);
+    }
+    //userInfo can be null
+    const username = userInfo?.username;
     return (
         <header>
         <Link to="/" className="logo">Logo</Link>
-
         <p><b> Welcome to MiniBlog </b></p>
         <nav>
-       
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+          {/* if we have username then next */}
+          {username && (
+          <>
+          <Link to="/create" > Create new Post </Link>
+          <a onClick={logout}> Logout</a>
+          </>
+          )}
+          {!username && (
+            <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+            </>
+          )}
         </nav>
       </header>
     );
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -5,24 +5,45 @@ export default function LoginPage() {
     const [emailID, setemailID] = useState('');
     const [password, setPassword] = useState('');
     const nav=useNavigate();
-
-    async function login(ev){
+    const {setUserInfo} = useContext(UserContext);
+    async function login(ev) {
         ev.preventDefault();
-        fetch('http://localhost:3000/auth/login',{
-            method: 'POST',
-            body: JSON.stringify({emailID,password}),
-            headers: {'Content-type':'application/json'},
-            // credentials: 'include',
+        const response = await fetch('http://localhost:4000/login', {
+          method: 'POST',
+          body: JSON.stringify({username, password}),
+          headers: {'Content-Type':'application/json'},
+        //   credentials: 'include',
+        });
+        if (response.ok) {
+          response.json().then(userInfo => {
+            //from JSON (backend)
+            setUserInfo(userInfo);
+            console.log("Login credentials ok.");
+            nav("/");
+            // setRedirect(true);
+          });
+        } else {
+            console.log("Login credentials ok.");
+            nav("/");
         }
-        ).then(res=>{
-            if(res.ok){
-                console.log("Login credentials ok.")
-                nav("/")
-            } else{      
-                alert("Login Failed - Wrong Credentials")
-            }
-        })
-    }
+      }
+    // async function login(ev){
+    //     ev.preventDefault();
+    //     fetch('http://localhost:3000/auth/login',{
+    //         method: 'POST',
+    //         body: JSON.stringify({emailID,password}),
+    //         headers: {'Content-type':'application/json'},
+    //         // credentials: 'include',
+    //     }
+    //     ).then(res=>{
+    //         if(res.ok){
+    //             console.log("Login credentials ok.")
+    //             nav("/")
+    //         } else{      
+    //             alert("Login Failed - Wrong Credentials")
+    //         }
+    //     })
+    // }
     return (
         <form className="login" onSubmit={login}>
             <h1>Login</h1>
