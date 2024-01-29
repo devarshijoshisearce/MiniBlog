f79655b6a54dd0d35e72778b170bcbbaccbfaa1b
Devarshi Joshi <devarshi.joshi@searce.com>
2024-01-29 16:24:32 +0530

    .

--- API/routers/Blogs.js
+++ API/routers/Blogs.js
@@ -6,12 +6,12 @@ const uploadMiddleWare  = multer({dest : 'uploads/'})
 
 const controller=require("../controllers/Blogs")
 
-router.post('/createBlog',uploadMiddleWare.single('file') , controller.createBlogs);                          //create a blog. data will be passed by a form
+router.post('/createBlog',uploadMiddleWare.single('files') , controller.createBlogs);                          //create a blog. data will be passed by a form
 router.get('/viewBlogs',controller.viewBlogs);                              //retrieve all blogs
 router.get('/viewBlogsbyID/:id',controller.viewBlogsbyID);                  //retrieve a specific post. Search by postID
 router.get('/viewBlogsbyAuthor/:writer',controller.viewBlogsbyAuthor);      //retrieve all posts by user name -> search function
 router.get('/viewBlogsbyAuthorID/:writerid',controller.viewBlogsbyAuthorID);//retrieve all post by a specific user -> user page
 router.put('/deleteBlogsbyID/',controller.deleteBlogsbyID);                 //delete a specific post -> pass postID, authorID, userID as params similar to how data was passed in creating a blog
-router.post('/updateBlogbyID', uploadMiddleWare.single('file'), controller.updateBlogsbyID)                      //update a post -> pass postID, authorID, userID, and the same data as createBLogs.
+router.post('/updateBlogbyID', uploadMiddleWare.single('files'), controller.updateBlogsbyID)                      //update a post -> pass postID, authorID, userID, and the same data as createBLogs.
 
 module.exports=router
